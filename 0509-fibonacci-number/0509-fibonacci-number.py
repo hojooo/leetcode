b87fd87be7bdf15cc01954b0914b2549ac881e30
Sparrow_j <112610295+hojooo@users.noreply.github.com>
2023-12-21 15:45:42 +0900

    Time: 40 ms (63.11%), Space: 17.4 MB (14.31%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if 0 < n <= 2:
+            return 1
+
+        dp = [0 for _ in range(n+1)]
+        dp[0] = 0
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[-1]
+
+#         dp = [-1 for _ in range(n+1)]
+        
+#         if dp[n] != -1:
+#             dp[n] = fibo()
